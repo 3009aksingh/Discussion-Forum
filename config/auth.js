7f5b99fce6847eb7cd22152fffb658aaa23565e2
Ankit Singh <3009aksingh@gmail.com>
2021-06-26 22:42:30 +0530

    edits

--- config/auth.js
+++ config/auth.js
@@ -1,16 +1,15 @@
 module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      req.flash('error_msg', 'Please log in to view that resource');
-      res.redirect('/users/login');
-    },
-    forwardAuthenticated: function(req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next();
-      }
-      res.redirect('/discussion1st');      
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
     }
-  };
- 
+    req.flash('error_msg', 'Please log in to view that resource');
+    res.redirect('/users/login');
+  },
+  forwardAuthenticated: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/discussion1st');
+  }
+};
